cba00e723fe1905c0dfd5fdca2f01786c1a8a505
CourseMaterial <55927635+CourseMaterial@users.noreply.github.com>
2022-03-09 23:30:16 +0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -7,10 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-west-2"
-}
-
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
@@ -18,7 +14,7 @@ resource "aws_vpc" "example" {
 resource "aws_subnet" "example" {
   vpc_id     = aws_vpc.example.id
   cidr_block = var.subnet
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "${var.servername}subnet"
