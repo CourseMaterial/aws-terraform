59537054a1a84452da89bb254cc7c70e522dacc2
CourseMaterial <55927635+CourseMaterial@users.noreply.github.com>
2022-03-10 09:48:41 +0500

    Delete ec2 directory

--- ec2/output.tf
+++
@@ -1,9 +0,0 @@
-output "private-ip" {
-    description = "Private IP Address of EC2"
-    value = aws_instance.instance.private_ip
-}
-
-output "public-ip" {
-  description = "Public IP Address of EC2"
-  value = aws_instance.instance.public_ip
-}
