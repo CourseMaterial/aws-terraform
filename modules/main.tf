82dbe45c6681c8ab543a79632b407b55397949f6
CourseMaterial <55927635+CourseMaterial@users.noreply.github.com>
2022-03-11 16:13:25 +0500

    Add files via upload

--- modules/main.tf
+++ modules/main.tf
@@ -1,12 +1,18 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.7"
-    }
-  }
-}
+
 
 provider "aws" {
-  region = "us-east-2"  
+    region = "eu-west-1"
+}
+
+module "work_queue" {
+    source = "./sqs-with-backoff"
+    queue_name = "work-queue"
+}
+
+output "work_queue_name" {
+    value = module.work_queue.queue_name
+}
+
+output "work_queue_dead_letter_name" {
+    value = module.work_queue.dead_letter_queue_name
 }
